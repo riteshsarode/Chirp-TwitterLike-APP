43cab07241335c05a4833e3c785ef4c80c6b3a29
riteshsarode <ritsarode@gmail.com>
2017-05-29 02:13:32 -0400

    Designed APIs to get data from the server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,19 @@ const app = express()   //server side app
 //Host the website on this server on port 3000 | Serve the front end front he folder public
 app.use(express.static('public'));
 
+//Make an API
+app.get('/chirps', function(req,res,next) {
+	var chirps = [
+		'Hi I am a new chirp! ',
+		'Wola!!! Wassup guys', 
+		'I just got a new brother', 
+		'Woho! Congratulations chirp',
+		'Hello again!'
+	];
+
+	res.send(chirps);
+})
+
 // Listen on port 3000 and after starting execute the function(second argument)
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
