1605a2e897635377be2a654ba882a746dcb43922
riteshsarode <ritsarode@gmail.com>
2017-05-29 23:03:23 -0400

    Connected with MongoDb database - Can read and write to DB

--- src/server.js
+++ src/server.js
@@ -1,20 +1,47 @@
-const express = require('express') 
-const app = express()   //server side app
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;  
+var bodyParser = require('body-parser');
+var app = express()   //server side app
 
 //Host the website on this server on port 3000 | Serve the front end front he folder public
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-//Make an API
+//Declaring a variable db
+var db = null;
+//Access chirpDB from the server
+// Connect the Database to this server ||| Database is 'chirpDb' and the collection inside the database is called 'allchirps'
+MongoClient.connect("mongodb://localhost:27017/chirpDb",function(err,dbConnection){
+	if (!err) {
+		console.log("Database is connected!!!");
+		db = dbConnection;
+	}
+});
+
+//Make an API.  // req contains the request object, res is response object to send data back, next is error object used to specify error
 app.get('/chirps', function(req,res,next) {
-	var chirps = [
-		'Hi I am a new chirp! ',
-		'Wola!!! Wassup guys', 
-		'I just got a new brother', 
-		'Woho! Congratulations chirp',
-		'Hello again!'
-	];
+	
+	db.collection('allchirps', function(err,chirpsCollection){
+		chirpsCollection.find().toArray(function(err,chirps){
+		
+		return res.json(chirps);
+		});
+	});
+})
 
-	res.send(chirps);
+//handles the post request from the front end  
+app.post('/chirps', function(req,res,next) {
+
+	// console.log(req.body.newChirp);
+	// chirps.push(req.body.newChirp);
+
+	db.collection('allchirps', function(err,chirpsCollection){
+		chirpsCollection.insert( {text: req.body.newChirp},{w:1},function(err,chirps){
+		
+		return res.send();
+		});
+	});
+
 })
 
 // Listen on port 3000 and after starting execute the function(second argument)
